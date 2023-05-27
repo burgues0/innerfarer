c821d25ac4e5057612d7566d2c5ab6e7f83c18b5
burgues0 <victordpp@outlook.com>
2023-05-27 15:49:12 -0300

    add: some logic

--- level.py
+++ level.py
@@ -25,11 +25,55 @@ class Level:
                     player_sprite = Player((cellX,cellY))
                     self.player.add(player_sprite)
 
-    def run(self):
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if(player_x < (WIDTH / 4) and direction_x < 0):
+            self.world_shift = 8
+            player.speed = 0
+        elif (player_x > (WIDTH - (WIDTH / 4)) and direction_x > 0):
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
+    def x_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if(sprite.rect.colliderect(player.rect)):
+                if(player.direction.x < 0):
+                    player.rect.left = sprite.rect.right
+                elif(player.direction.x > 0):
+                    player.rect.right = sprite.rect.left
+
+    def y_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if(sprite.rect.colliderect(player.rect)):
+                if(player.direction.y > 0):
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                elif(player.direction.y < 0):
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0
+
+    def run(self): 
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.scroll_x()
+
         self.player.update()
+        self.x_movement_collision()
+        self.y_movement_collision()
         self.player.draw(self.display_surface)
+
 
 # def get_bg(theme):
 #     image = pygame.image.load(join("assets", "tiles", "background", theme)).convert_alpha()
